4322e555bd9bca91a6d9d46920c258b7f296912b
greatti <greattibrenno@gmail.com>
2021-03-23 10:28:09 -0300

    Update project_01.py
    Add the context of the problem.

--- project_01.py
+++ project_01.py
@@ -17,3 +17,29 @@ pd.options.display.max_rows = None
 
 df = pd.read_csv('NISPUF17.csv') #Please, note that the csv file need to be at the same directory as your code file, or you can copy the path
 print(df.head())
+
+'''
+Notice that we dont have idea of what all this variables in columns represent, so we need to study the ones
+we want to work with, and this is part of the work of a data scientist too, so open the NIS-PUF17-DUG.pdf 
+and read about the variables we will work first
+
+In this first part we want to discover the PROPROTION OF CHILDREN who had the MOTHER with EDUCATION LEVELS 
+equal to: LESS THAN HIGHSCHOOL, EQUAL TO HIGHSCHOOL, MORE THAN HIGHSCHOOL BUT NOT COLLEGE GRADUATE and
+COLLEGE DEGREE
+'''
+
+'''
+Page 55 says: "The age, education level, and marital status of the mother of the child are
+stored in variables M_AGEGRP2, EDUC1, and MARITAL2 (married vs. not married), with missing
+values imputed."
+
+That means that our mother education level is stored in EDUC1 variable, and that is confirmed in page 51:
+
+EDUC1 – education of the mother : 
+                                - <12 years
+                                - 12 years
+                                - >12 years, not a college graduate
+                                - College graduate
+
+So the thing is: we need to get each one of this data and divide for the total of mothers
+'''
