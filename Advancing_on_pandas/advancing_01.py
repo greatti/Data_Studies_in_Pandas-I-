cca2b3f6005f4b3faf2fbe859ac6a4e08005098d
greatti <greattibrenno@gmail.com>
2021-04-04 22:57:47 -0300

    Update advancing_01.py
    Add merge() function with Outer method.

--- Advancing_on_pandas/advancing_01.py
+++ Advancing_on_pandas/advancing_01.py
@@ -23,3 +23,18 @@ students_df = students_df.set_index('Name')
 print(staff_df)
 print(students_df)
 
+'''Lets merge it using merge():
+merge() is a function that join two dataframes on columns or indexes
+Arguments of merge() method can be found in https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.merge.html?highlight=merge#pandas.DataFrame.merge
+and i cannot explain all of them in here, just the ones i'll use
+'''
+
+outer_df = pd.merge(staff_df, students_df, how = 'outer', left_index = True, right_index = True)
+'''Lets explain all of this: 
+merge is a function that join two dataframes, as we can see, in this case, the first two arguments; 
+'how' parameter is the method, and 'outer' represent that we'll use keys from both frames
+'left_index' is a parameter that ask if we want to use the index of the first frame, in this case is True
+'right_index' same as left
+'''
+print(outer_df) #You need to observe that Lewis is in ['Role'] but not in ['School'], and the same happens to Utida
+
