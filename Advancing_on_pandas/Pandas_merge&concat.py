2c79a3de038f41aa22183e88bcd298eb28c7dee3
greatti <greattibrenno@gmail.com>
2021-04-09 17:53:59 -0300

    Update Pandas_merge&concat.py
    Add #### to print statements

--- Advancing_on_pandas/Pandas_merge&concat.py
+++ Advancing_on_pandas/Pandas_merge&concat.py
@@ -20,8 +20,8 @@ not in both dataframes'''
 staff_df = staff_df.set_index('Name')
 students_df = students_df.set_index('Name')
 
-print(staff_df)
-print(students_df)
+#### print(staff_df)
+#### print(students_df)
 
 '''Lets merge it using merge():
 merge() is a function that join two dataframes on columns or indexes
@@ -36,20 +36,20 @@ merge is a function that join two dataframes, as we can see, in this case, the f
 'left_index' is a parameter that ask if we want to use the index of the first frame, in this case is True
 'right_index' same as left
 '''
-print(outer_df) #You need to observe that Lewis is in ['Role'] but not in ['School'], and the same happens to Utida
+#### print(outer_df) #You need to observe that Lewis is in ['Role'] but not in ['School'], and the same happens to Utida
 
 '''
 If we wanted just the data that are in both frames, than we do a inner join, and we wxpect to stay with just
 two rows and no missing values
 '''
 inner_df = pd.merge(staff_df, students_df, how = 'inner', left_index = True, right_index = True)
-print(inner_df)
+#### print(inner_df)
 
 ''' And we can even add the left frame elements to the right frame, or the opposite '''
 right_df = pd.merge(staff_df, students_df, how = 'right', left_index = True, right_index = True)
 left_df = pd.merge(staff_df, students_df, how = 'left', left_index = True, right_index = True)
-print(right_df)
-print(left_df)
+#### print(right_df)
+#### print(left_df)
 
 '''Notice that in the right join we have just 'students_df' elements, and in the left join we have just
 'staff_df' elements
@@ -58,7 +58,7 @@ Do you remember that we had to set the indexes before merging? We dont need to d
 
 '''
 join_on = pd.merge(staff_df, students_df, how = 'right', on = 'Name')
-print(join_on)
+#### print(join_on)
 
 ''' What if we had conflict between frames?'''
 
@@ -70,13 +70,13 @@ studentsdf = pd.DataFrame([{'Name' : 'Greatti', 'School' : 'UEM', 'Location' : '
                            {'Name' : 'Fuzioka', 'School' : 'Cesu', 'Location' : 'Guedner Avenue'},
                            {'Name' : 'Currie', 'School' : 'PUC', 'Location' : 'Duque'}])
 
-print(staffdf)
-print(studentsdf)
+#### print(staffdf)
+#### print(studentsdf)
 
 ''' We want all the information about people on staff, that is, the info in staffdf and their
 info in studentsdf in case there is any student at staff '''
 info_staff = pd.merge(staffdf, studentsdf, how = 'left', on = 'Name')
-print(info_staff)
+#### print(info_staff)
 ''' See that because we had two different columns named 'Location', Pandas atributted _x and _y sufixes '''
 
 '''What if the first name of two people are equal but not the last name? '''
@@ -92,7 +92,7 @@ studentsdf = pd.DataFrame([{'Fname' : 'Brenno', 'Lname' : 'Bethe', 'School' : 'M
 #Brenno Bethe and Joui Jouki arent in staffdf, but Larissa Reis is
 
 Intersec = pd.merge(staffdf, studentsdf, how = 'inner', on = ['Fname', 'Lname'])
-print(Intersec) #And now we see that the only intersection is Larissa Reis, that are on both frames in index
+#### print(Intersec) #And now we see that the only intersection is Larissa Reis, that are on both frames in index
 
 '''
 Lets look to a lot of datasets: 
@@ -107,20 +107,20 @@ df2011 = pd.read_csv('MERGED2011_12_PP.csv', error_bad_lines = False) #error_bad
 df2012 = pd.read_csv('MERGED2012_13_PP.csv', error_bad_lines = False)
 df2013 = pd.read_csv('MERGED2013_14_PP.csv', error_bad_lines = False)
 
-print(df2011.head())
-print(df2012.head())
-print(df2013.head())
+#### print(df2011.head())
+#### print(df2012.head())
+####print(df2013.head())
 #If we want to see the length of the dataframes:
-print(len(df2011))
-print(len(df2012))
-print(len(df2013))
+####print(len(df2011))
+####print(len(df2012))
+####print(len(df2013))
 
 #We can now create a list with the three dataframes to concatenate them
 
 frames = [df2011, df2012, df2013]
 conc = pd.concat(frames)
 #And now the total length is:
-print(len(df2011) + len(df2012) + len(df2013))
+#### print(len(df2011) + len(df2012) + len(df2013))
 
 '''
 Although, a problem in concat() is that we cannot know from which dataframe is each element.
@@ -129,7 +129,7 @@ We can add another level of index that will inform the year
 '''
 
 f_conc = pd.concat(frames, keys = ['2011', '2012', '2013'])
-print(f_conc) # and that way we associate wach element of the list with the correspondent key
+#### print(f_conc) # and that way we associate wach element of the list with the correspondent key
 
 '''
 full pandas.concat() documentation can be found in here: https://pandas.pydata.org/docs/reference/api/pandas.concat.html?highlight=concat#pandas.concat
