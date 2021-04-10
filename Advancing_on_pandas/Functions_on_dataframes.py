fcf57503b27560e729ae1473e53d533cf64e65fa
greatti <greattibrenno@gmail.com>
2021-04-10 11:02:26 -0300

    Update Functions_on_dataframes.py
    Update and review some definitions

--- Advancing_on_pandas/Functions_on_dataframes.py
+++ Advancing_on_pandas/Functions_on_dataframes.py
@@ -26,7 +26,7 @@ df = (df.where(df['SUMLEV'] == 50)
 #### print(df.head())
 
 '''
-We can question: how fast is realiza all this processes at once when comparad to doing one at a time?
+We can question: how fast it is to realize all this processes at once when comparad to doing one at a time?
 
 For that we use 'timeit' library in two different functions
 '''
@@ -49,13 +49,14 @@ def second_approach(): #Doing one action at a time in 'df'
 df = pd.read_csv('C:/Users/great/Documents/GitHub/Data_studies/Advancing_on_pandas/census.csv')
 #### print(timeit.timeit(second_approach, number = 10))
 
-#Apparently the second method is 4 times faster, even the first one being leaner
+'''Apparently the second method is 4 times faster, even the first one being leaner, so, if you are heading
+a big study or a big analysis, maybe it is better if you do one step at a time in terms of velocity'''
 
 # =================================================================================================================#
 
 ''' We can clean all the variables now, because we'll explore map()
 
-When we want to use map(), we gotta pass to this some function and some iterable. The function, then, is
+When we want to use map(), we gotta pass to it some function and some iterable. The function, then, is
 applied to all the elements in this iterable and return a list of results
 
 Map in pandas is called by 'applymap()', you need to enter a function to be applied in each cell of the 
@@ -82,8 +83,11 @@ def min_max(row):
                 'POPESTIMATE2012', 
                 'POPESTIMATE2013', 
                 'POPESTIMATE2014', 
-                'POPESTIMATE2015',]] #When we call min_max() we create a frame called data with 6 rows
+                'POPESTIMATE2015',]] 
     return pd.Series({'min' : np.min(data), 'max' : np.max(data)}) 
+    #we are creating a Series with two columns:
+        #min is calculating the minimum value of each row
+        #max is calculating the maximum value of each row
 
 '''the function return a Serie with two columns based on 'data' data, filtering the min and the max of each row
 
@@ -93,8 +97,9 @@ Now, we need to use 'Apply()' in df using min_max():
                                 -Axis: which axis is the index? 'columns' or 'rows'? 
 '''
 
-dff = df.apply(min_max, axis = 'columns').head()
+dff = df.apply(min_max, axis = 'columns').head() 
 #### print(dff)
+#will apply min_max() function on columns of 'df' and return a Series with this two columns only
 
 ''' What if we wanted to incorporete this two columns to census.csv? '''
 
@@ -108,24 +113,26 @@ def min_max2(row):
     row['max'] = np.max(data) #create a row called 'max' that is the maximum value of 'data'
     row['min'] = np.min(data) #create a row called 'min' that is the minimum value of 'data'
     
-    return row #return all the rows
+    return row #return this two new rows
 
-dfff = df.apply(min_max2, axis = 'columns').head()
+dfff = df.apply(min_max2, axis = 'columns').head() 
 #### print(dfff)
+#will apply the function on 'df' and incorporate this columns to the dataframe
 
 '''
 All this things we did can be done in a single line using lambda function
 '''
+
 rows = ['POPESTIMATE2010', 'POPESTIMATE2011', 'POPESTIMATE2012', 'POPESTIMATE2013', 'POPESTIMATE2014', 'POPESTIMATE2015']
-dfmax = df.apply(lambda x: np.max(x[rows]), axis = 'columns').head() #we are applying np.max on 'rows' list of columns
+dfmax = df.apply(lambda x: np.max(x[rows]), axis = 'columns').head() #we are applying np.max on 'rows' list of columns of df
 dfmin = df.apply(lambda x: np.min(x[rows]), axis = 'columns').head()
-print(dfmax)
-print(dfmin)
+#### print(dfmax)
+#### print(dfmin)
 
 '''
 lambda: 
         -This is like a unnamed function that gets a unique variable 'x' and return
-        a unique value, in this case, the maximum value of row
+        a unique value, in this case, the maximum value of each element on the list
 
 apply is a cool way to go because we can use whatever function we defined previously and use whatever 
 function we want with lambda
