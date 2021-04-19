0c10da860e2c5adc3986d1b1ca9621a4e0d77434
greatti <greattibrenno@gmail.com>
2021-04-19 16:31:35 -0300

    Update categ( )_cut( ).py
    Add an introduction to pivot tables

--- Advancing_on_pandas/categ( )_cut( ).py
+++ Advancing_on_pandas/categ( )_cut( ).py
@@ -47,8 +47,73 @@ df = df.set_index('STNAME').groupby(level = 0)['CENSUS2010POP'].agg(np.average)
 our dataframe is now super reduced, because we defined the STATE NAME as index and calculated the average over the 
 2010 POPULATION in each state
 
-We can use cut(df, x) method to separate the data into groups of x elements
+We can use cut(df, x) method to separate the data into x bins of equal sizing, For example, cut could convert ages
+to groups of age ranges.
 '''
 
 dfcut = pd.cut(df, 10)
-print(dfcut)
+#### print(dfcut)
+
+###### PIVOT_TABLES ######
+
+'''
+Pivot tables is a way of sumarizing data in a dataframe by a specific motive:
+'''
+
+import pandas as pd 
+import numpy as np
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
+df = pd.read_csv('C:/Users/great/Documents/GitHub/Data_studies/Advancing_on_pandas/cwurData.csv')
+#### print(df.head())
+
+''' lets first create a new column, where if the university rank is 01-101, it is classified as first tier
+if the university rank is 102-202 it is classified as second tier
+if the univerity rank is 203-303 it is classified as third tier
+the other are fourth rank
+
+reading this you can imagine the function to do this, right? It is a if function that returns which tier
+depending on rank 
+'''
+
+def create_category(ranking): 
+    if (ranking >=1) & (ranking <=101):
+        return 'FirstTier'
+    if (ranking >101) & (ranking <=202): 
+        return 'SecondTier' 
+    if (ranking > 201) & (ranking <=303):
+        return 'ThirdTier'
+    else:
+        return 'FourthTier'
+
+''' To apply this function we dont have any problem, we'll use apply( ) with lambda '''
+
+df['Tier'] = df['world_rank'].apply(lambda x: create_category(x))
+#### print(df['Tier'].head(30)) #lets see this column, it seems it is already organized by order of tier
+
+''' Lets suppose we want now to see the relationship between Tier and Country in terms of Score
+
+values = Score
+index = country
+columns = rank_level '''
+
+dfpiv = df.pivot_table(values = 'score', index = 'country', columns = 'Tier', aggfunc = [np.mean])
+#### print(dfpiv.head(30))
+
+'''
+basically, what are we doing? We are getting the mean value (function) of 'Score'(values) in each Tier(columns)
+of each country(index)
+
+You noticed that we have NaN data? That means, for example, that Argentina dont have 'Score' values to apply the 
+function in this classification (Tier)
+
+pivot.table( ) isnt limited to one function, you can assign one or more: 
+'''
+
+dfpivt = df.pivot_table(values = 'score', index = 'country', columns = 'Tier', aggfunc = [np.mean, np.max]).head()
+#### print(dfpivt)
+
+dfpivtma = df.pivot_table(values = 'score', index = 'country', columns = 'Tier', aggfunc = [np.mean, np.max], margins = True)
+#margins = True show the superior limit 
+print(dfpivtma)
